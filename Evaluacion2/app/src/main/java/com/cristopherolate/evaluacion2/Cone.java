a00c16dcb9c3590b7a85dc9f151e5054c401b777
CristopherOlate <90350575+CristopherOlate@users.noreply.github.com>
2021-11-18 16:52:54 -0300

    Evaluacion 2 Android

---
+++ Evaluacion2/app/src/main/java/com/cristopherolate/evaluacion2/Cone.java
@@ -0,0 +1,34 @@
+package com.cristopherolate.evaluacion2;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class Cone extends SQLiteOpenHelper {
+
+    final String CREAR_TABLA_SENSORES = "CREATE TABLE sensores (nombre_sensor TEXT, valor_sensore TEXT, fecha_hora TEXT)";
+
+    final String CREAR_TABLA_MAPS = "CREATE TABLE mapa (lugar TEXT, datos TEXT)";
+
+    public Cone(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREAR_TABLA_SENSORES);
+        db.execSQL(CREAR_TABLA_MAPS);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS sensores");
+
+        db.execSQL("DROP TABLE IF EXISTS mapa");
+
+        onCreate(db);
+    }
+}
+
